746e82a57bfe2c9fe7b9e503bf337527e247d3fe
Cloud Han <cloudhan@outlook.com>
2020-10-25 12:01:55 +0800

    normalize data

--- hw1/solve.cpp
+++ hw1/solve.cpp
@@ -3,21 +3,69 @@
 #include <Eigen/Dense>
 #include <iostream>
 
-LeastSquare::LeastSquare()
-    : m{-1} {}
+Normalizer::Normalizer(const std::vector<Point>& points)
+    : mean_x{0}
+    , mean_y{0}
+    , std_x{0}
+    , std_y{0}
+{
+    for (const auto& p : points) {
+        mean_x += p.x;
+        mean_y += p.y;
+    }
+    mean_x /= points.size();
+    mean_y /= points.size();
+
+    for (const auto& p : points) {
+        std_x += (p.x - mean_x) * (p.x - mean_x);
+        std_y += (p.y - mean_y) * (p.y - mean_y);
+    }
+    std_x /= points.size() - 1;
+    std_y /= points.size() - 1;
+    std_x = sqrt(std_x);
+    std_y = sqrt(std_y);
+}
+
+float Normalizer::normalize_x(float x)
+{
+    return (x - mean_x) / std_x;
+}
+
+float Normalizer::normalize_y(float y)
+{
+    return (y - mean_y) / std_y;
+}
+
+float Normalizer::denormalize_x(float x)
+{
+    return x * std_x + mean_x;
+}
+
+float Normalizer::denormalize_y(float y)
+{
+    return y * std_y + mean_y;
+}
 
 LeastSquare::LeastSquare(int m, const std::vector<Point>& points)
-    : m{m} {
+    : norm{points}
+    , m{m}
+{
     Matrixf A;
     Vectorf b;
+
+    float x;
+    float y;
 
     A.resize(points.size(), m + 1);
     b.resize(points.size());
     for (int i = 0; i < points.size(); i++) {
+        x = norm.normalize_x(points[i].x);
+        y = norm.normalize_y(points[i].y);
+
         A(i, 0) = 1.0;
-        b(i)    = points[i].y;
+        b(i) = y;
         for (int j = 1; j < m + 1; j++) {
-            A(i, j) = A(i, j - 1) * points[i].x;
+            A(i, j) = A(i, j - 1) * x;
         }
     }
 
@@ -29,9 +77,10 @@ LeastSquare::LeastSquare(int m, const std::vector<Point>& points)
     }
 }
 
-std::vector<Point> LeastSquare::predict(float x_start, float x_end, int num_points) {
+std::vector<Point> LeastSquare::predict(float x_start, float x_end, int num_points)
+{
     auto step = (x_end - x_start) / (num_points - 1);
-    float x   = x_start;
+    float x = x_start;
 
     std::vector<Point> ret;
     ret.resize(num_points);
@@ -39,42 +88,47 @@ std::vector<Point> LeastSquare::predict(float x_start, float x_end, int num_poin
     Matrixf A(num_points, m + 1);
     for (int i = 0; i < num_points; i++, x += step) {
         ret[i].x = x;
-        A(i, 0)  = 1.0;
+
+        float normalized_x = norm.normalize_x(x);
+        A(i, 0) = 1.0;
         for (int j = 1; j < m + 1; j++) {
-            A(i, j) = A(i, j - 1) * x;
+            A(i, j) = A(i, j - 1) * normalized_x;
         }
     }
     Vectorf ys = A * coeff;
     for (int i = 0; i < num_points; i++) {
-        ret[i].y = ys(i);
+        ret[i].y = norm.denormalize_y(ys(i));
     }
     return ret;
 }
 
-RidgeRegression::RidgeRegression()
-    : m{-1} {}
-
 RidgeRegression::RidgeRegression(int m, float a, const std::vector<Point>& points)
-    : m{m}
-    , a{a} {
+    : norm{points}
+    , m{m}
+    , a{a}
+{
     Matrixf A;
     Vectorf b;
+
+    float x;
+    float y;
 
     A.resize(points.size(), m + 1);
     b.resize(points.size());
     for (int i = 0; i < points.size(); i++) {
+        x = norm.normalize_x(points[i].x);
+        y = norm.normalize_y(points[i].y);
+
         A(i, 0) = 1.0;
-        b(i)    = points[i].y;
+        b(i) = y;
         for (int j = 1; j < m + 1; j++) {
-            A(i, j) = A(i, j - 1) * points[i].x;
+            A(i, j) = A(i, j - 1) * x;
         }
     }
 
     if (points.size()) {
         Matrixf AT_A = A.transpose() * A;
-        std::cout << "before" << AT_A.diagonal() << std::endl;
         AT_A.diagonal().array() += a;
-        std::cout << "After" << AT_A.diagonal() << std::endl;
         if (a > 0) {
             coeff = AT_A.llt().solve(A.transpose() * b);
         } else {
@@ -86,9 +140,10 @@ RidgeRegression::RidgeRegression(int m, float a, const std::vector<Point>& point
     }
 }
 
-std::vector<Point> RidgeRegression::predict(float x_start, float x_end, int num_points) {
+std::vector<Point> RidgeRegression::predict(float x_start, float x_end, int num_points)
+{
     auto step = (x_end - x_start) / (num_points - 1);
-    float x   = x_start;
+    float x = x_start;
 
     std::vector<Point> ret;
     ret.resize(num_points);
@@ -96,14 +151,16 @@ std::vector<Point> RidgeRegression::predict(float x_start, float x_end, int num_
     Matrixf A(num_points, m + 1);
     for (int i = 0; i < num_points; i++, x += step) {
         ret[i].x = x;
-        A(i, 0)  = 1.0;
+
+        float normalized_x = norm.normalize_x(x);
+        A(i, 0) = 1.0;
         for (int j = 1; j < m + 1; j++) {
-            A(i, j) = A(i, j - 1) * x;
+            A(i, j) = A(i, j - 1) * normalized_x;
         }
     }
     Vectorf ys = A * coeff;
     for (int i = 0; i < num_points; i++) {
-        ret[i].y = ys(i);
+        ret[i].y = norm.denormalize_y(ys(i));
     }
     return ret;
 }
--- hw1/solve.hpp
+++ hw1/solve.hpp
@@ -7,22 +7,38 @@
 using Matrixf = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>;
 using Vectorf = Eigen::Matrix<float, Eigen::Dynamic, 1>;
 
+struct Normalizer {
+    float mean_x;
+    float mean_y;
+    float std_x;
+    float std_y;
+    Normalizer() {}
+    Normalizer(const std::vector<Point>& points);
+
+    float normalize_x(float x);
+    float normalize_y(float y);
+
+    float denormalize_x(float x);
+    float denormalize_y(float y);
+};
 
 // basis function: 1, x, x^2, ...
 
 struct LeastSquare {
-    int m; // the highest order of the basis function
+    Normalizer norm;
+    int m;         // the highest order of the basis function
     Vectorf coeff; // the solved coefficient
-    LeastSquare();
+    LeastSquare() {}
     LeastSquare(int m, const std::vector<Point>& points);
     std::vector<Point> predict(float x_start, float x_end, int num_points);
 };
 
 struct RidgeRegression {
+    Normalizer norm;
     int m;
     float a; // the weighting term of normalization
     Vectorf coeff;
-    RidgeRegression();
+    RidgeRegression() {}
     RidgeRegression(int m, float a, const std::vector<Point>& points);
     std::vector<Point> predict(float x_start, float x_end, int num_points);
 };
