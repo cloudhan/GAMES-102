b3f502f29e9d77506fee934af851b42bdc4e5095
Cloud Han <cloudhan@outlook.com>
2020-11-22 11:14:28 +0800

    no need to hack imgui from vcpkg anymore

--- hw1/gui.hpp
+++ hw1/gui.hpp
@@ -4,18 +4,8 @@
 #define GLFW_INCLUDE_NONE 1
 #include <imgui.h>
 
-#if __has_include("bindings/imgui_impl_glfw.h")
-#define USE_VCPKG 1
-#endif
-
-#if USE_VCPKG
-// headers from vcpkg
-#include "bindings/imgui_impl_glfw.h"
-#include "bindings/imgui_impl_opengl3.h"
-#else
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
-#endif
 
 #include <glbinding/gl/gl.h>
 #include <glbinding/glbinding.h>
--- hw1/imgui_impl.cpp
+++
@@ -1,6 +0,0 @@
-#include "gui.hpp"
-
-#if USE_VCPKG
-#include "bindings/imgui_impl_glfw.cpp"
-#include "bindings/imgui_impl_opengl3.cpp"
-#endif
